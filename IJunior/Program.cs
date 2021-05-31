2e41cdb7775c7a9ebae3b206f138a4ba7e6f34b6
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-05-31 21:08:53 +0300

    HomeTask1

--- IJunior/Program.cs
+++ IJunior/Program.cs
@@ -6,7 +6,20 @@ namespace IJunior
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Vlad";
+            string secondName = "Boshov";
+            int age = 25;
+            bool isIamCorrect = true;
+            byte bit = 8;
+            sbyte badBit = -8;
+            char a = 'A';
+            double weight = 78.65;
+            float distance = 12345f;
+            short notBigDisatance = 720;
+            
+
+            
+
         }
     }
 }
