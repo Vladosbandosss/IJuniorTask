efff78644c7589adb0c361eaaaf1ced5c181407e
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-05-31 21:22:11 +0300

    HomeTaskPicture

---
+++ HomeTaskPicture/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeTaskPicture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPicture = 52;
+            int pictureInColuum = 3;
+            int howMuchColumWeCanDo = allPicture / pictureInColuum;
+            Console.WriteLine($"Всего мы сможем сделать {howMuchColumWeCanDo} колонок");
+            int howMuchWeDone = howMuchColumWeCanDo * pictureInColuum;
+            int pictureLose = allPicture - howMuchWeDone;
+            Console.WriteLine($"Осталось лишней {pictureLose} картинка");
+        }
+    }
+}
