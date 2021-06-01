637cc8ce50966dde877692dcc805b2f990a5a824
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-01 12:52:02 +0300

    PoliklinTask

--- IJunior/Program.cs
+++ IJunior/Program.cs
@@ -6,21 +6,14 @@ namespace IJunior
     {
         static void Main(string[] args)
         {
-            //Gold Task
-            Console.WriteLine("Введите количесто золота которое у вас есть");
-            int myGold =Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine("у меня есть " + myGold + " золотых монет");
-            double crystalPrice = 2.55;
-            Console.WriteLine("Курс обмена составляет "+ crystalPrice + " за 1 золотую монету");
-            double howMuchCrystalWaHave = 100;
-            Console.WriteLine("У меня есть " + howMuchCrystalWaHave + "кристалов");
-            Console.WriteLine("Сколько золота хотите поменять?");
-            int howMuchGoldChange = Convert.ToInt32(Console.ReadLine());
-            double crystalResult = howMuchGoldChange * crystalPrice;
-            Console.WriteLine("готово,вы отдали " + howMuchGoldChange + " золотых монеток и взамен получили " + Math.Round(crystalResult, 2) + " кристалов");
-             myGold -= howMuchGoldChange;
-            howMuchCrystalWaHave -=  Math.Round(crystalResult, 2);
-            Console.WriteLine("У нас осталось " + myGold + " золотых монеток и " + howMuchCrystalWaHave + " кристаллов");
+         
+            Console.WriteLine("Введите количество людей перед вами");
+            int countOfPeople = Convert.ToInt32(Console.ReadLine());
+            int timeresultInMinuters = countOfPeople * 10;
+            TimeSpan findRes = TimeSpan.FromMinutes(timeresultInMinuters);
+            Console.WriteLine("Вам стоять " + findRes.Hours + " часов " + findRes.Minutes + " минут");
+
+            
             
 
             
