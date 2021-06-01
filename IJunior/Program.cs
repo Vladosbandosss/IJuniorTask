7b73e4e59f064660b35a4ecd46dd8678024821bf
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-01 13:36:09 +0300

    stringTask

--- IJunior/Program.cs
+++ IJunior/Program.cs
@@ -7,12 +7,17 @@ namespace IJunior
         static void Main(string[] args)
         {
 
-            int minutesOfWaitingPerOnePeople = 10;
-            Console.WriteLine("Введите количество людей перед вами");
-            int countOfPeopleBeforeMe = Convert.ToInt32(Console.ReadLine());
-            int AllminutesOfWaiTing = countOfPeopleBeforeMe * minutesOfWaitingPerOnePeople;
-            TimeSpan findRes = TimeSpan.FromMinutes(AllminutesOfWaiTing);
-            Console.WriteLine("Вам стоять " + findRes.Hours + " часов " + findRes.Minutes + " минут");
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваше место работы");
+            string placeOfWork = Console.ReadLine();
+            Console.WriteLine("Введите вашу зарплату");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Спасибо,вывожу информацию о вас");
+            Console.WriteLine($"Вас зовут {name}, вам {age} лет. Вы работаете {placeOfWork} ,а ваша зарплата {salary} ");
+            Console.WriteLine($"Кстати через год вам будет {age+1} и если к вашей зарплате прибавить 200 долларов ,то вы будете получать {salary+200}");
 
             
             
