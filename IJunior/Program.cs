3263a43d49825d60a6f94b993cb87f79639dafc5
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-01 13:23:47 +0300

    Правка

--- IJunior/Program.cs
+++ IJunior/Program.cs
@@ -6,11 +6,12 @@ namespace IJunior
     {
         static void Main(string[] args)
         {
-         
+
+            int minutesOfWaitingPerOnePeople = 10;
             Console.WriteLine("Введите количество людей перед вами");
             int countOfPeopleBeforeMe = Convert.ToInt32(Console.ReadLine());
-            int minutesOfWaiTing = countOfPeopleBeforeMe * 10;
-            TimeSpan findRes = TimeSpan.FromMinutes(minutesOfWaiTing);
+            int AllminutesOfWaiTing = countOfPeopleBeforeMe * minutesOfWaitingPerOnePeople;
+            TimeSpan findRes = TimeSpan.FromMinutes(AllminutesOfWaiTing);
             Console.WriteLine("Вам стоять " + findRes.Hours + " часов " + findRes.Minutes + " минут");
 
             
