e99683505d4de4b638006b72ecd8d1a2f35b8cb6
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-03 13:43:39 +0300

    таск

--- IJunior/Program.cs
+++ IJunior/Program.cs
@@ -6,94 +6,61 @@ namespace IJunior
     {
         static void Main(string[] args)
         {
-            double myUsd = 100;
-            double myEur = 100;
-            double myRub = 100;
-            bool makeChange = true;
+            bool workApp = true;
+            int appPasword = 12345;
             
-            while (makeChange)
+            while (workApp)
             {
-                Console.WriteLine($"Добро пожаловать в обменник! У тебя есть {myRub} рублей,{myUsd} долларов,{myEur} евро");
-                Console.WriteLine("Курс Евро/Доллар/Рубль равняется 1/2/4");
-                Console.WriteLine("Выбери что надо сделать!");
-                Console.WriteLine("1-обмен EUR на USD! 1 евро = 2 доллара");
-                Console.WriteLine("2-обмен  EUR на RUB! 1 евро = 4 рубля");
-                Console.WriteLine("3-обмен USD на EUR! 1 доллар = 0.5 евро");
-                Console.WriteLine("4- обмен USD на RUB!1 доллар = 2 рубля");
-                Console.WriteLine("5- обмен RUB на USD!1 рубль = 0.5 доллара");
-                Console.WriteLine("6- обмен RUB на EUR!1 рубль = 0.25 евро");
-                Console.WriteLine("Любая другая цифра заканчиваю работу");
-                int ourChoose = Convert.ToInt32(Console.ReadLine());
-               
-                    switch (ourChoose)
-                    {
-                        case 1:
-                            Console.WriteLine("Сколько евро хотите поменять на доллары?");
-                            int eurInUsd = Convert.ToInt32(Console.ReadLine());
-                            int exchangeEURinUsd = eurInUsd * 2;
-                            myEur -= eurInUsd;
-                            myUsd += exchangeEURinUsd;
-                            Console.WriteLine("Готово");
-                           break;
-                        case 2:
-                            Console.WriteLine("Сколько евро хотите поменять на рубли?");
-                            int eurInRub = Convert.ToInt32(Console.ReadLine());
-                            int exchangeEurInRub = eurInRub * 4;
-                            myEur -= eurInRub;
-                            myRub += exchangeEurInRub;
-                            break;
-                        case 3:
-                            Console.WriteLine("Сколько долларов хотите поменять на евро?");
-                            int usdInEur = Convert.ToInt32(Console.ReadLine());
-                            double exchangeUsdInEUR = usdInEur * 0.5;
-                            myUsd -= usdInEur;
-                            myEur += exchangeUsdInEUR;
-                            break;
-                        case 4:
-                            Console.WriteLine("Сколько долларов хотите поменять на рубли?");
-                            int usdInRuble = Convert.ToInt32(Console.ReadLine());
-                            double exchangrUsdInRub = usdInRuble * 2;
-                            myUsd -= usdInRuble;
-                            myRub += exchangrUsdInRub;
-                            break;
-                        case 5:
-                            Console.WriteLine("Сколько рублей хотите поменять на доллары?");
-                            int rubleInUsd = Convert.ToInt32(Console.ReadLine());
-                            double exchangeRubleInUsd = rubleInUsd * 0.5;
-                            myRub -= rubleInUsd;
-                            myUsd += exchangeRubleInUsd;
-                            break;
-                        case 6:
-                            Console.WriteLine("Сколько рублей хотите поменять на евро?");
-                            int rubleInEur = Convert.ToInt32(Console.ReadLine());
-                            double exchangeRubleInEur = rubleInEur * 0.25;
-                            myRub -= rubleInEur;
-                            myEur += exchangeRubleInEur;
-                            break;
+                Console.WriteLine("Выбери команду а я исполню!");
+                Console.WriteLine("Нажми 1 что б изменить название консоли");
+                Console.WriteLine("Нажми 2 что б изменить цвет консоли");
+                Console.WriteLine("Нажми 3,введешь свое имя и я выведу его 3 раза");
+                Console.WriteLine("нажми 4 и введи пароль и я расскажу историю");
+                Console.WriteLine("Нажми другую цифру любую и прекратим");
+                int numberChoose = Convert.ToInt32(Console.ReadLine());
+                switch (numberChoose)
+                {
+                    case 1:
+                        Console.WriteLine("Веди новое название для консоли");
+                        string consoleNewName = Console.ReadLine();
+                        Console.Title =consoleNewName;
+                        Console.WriteLine("Готово название изменено");
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Готово цвет консоли изменен");
+                        break;
+                    case 3:
+                        Console.WriteLine("Введи имя");
+                        string name = Console.ReadLine();
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.Write(name + " ");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Веди пароль,надеюсь ты его не забыл");
+                        int myPassword = Convert.ToInt32(Console.ReadLine());
+                        if (appPasword == myPassword)
+                        {
+                            Console.WriteLine("Верный пароль,но я уже забыл истроию(");
 
-                        default:
-                            Console.WriteLine("Выхожу с паботы");
-                        makeChange = false;
-                            break;
-                    }
-                Console.WriteLine("Работа окончена");
-                
+                        }
+                        else
+                        {
+                            Console.WriteLine("Пароль не верный");
+                        }
+                        
+                        break;
+                        
+                     default:
+                        Console.WriteLine("Жаль,что ты решил прекратить работу,пока");
+                        workApp = false;
+                        break;
                 }
-               
-
-
-
-
-
-
-
-
-
-
-
-
-
-            }
+             }
+                Console.WriteLine("Спасибо,работа окончена");
+             }
            
         }
     }
