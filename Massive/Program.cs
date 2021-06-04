10a1cfcd165b169d2e06d88d3010fbea3e0a2a5e
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-04 12:30:54 +0300

    task2

--- Massive/Program.cs
+++ Massive/Program.cs
@@ -7,26 +7,30 @@ namespace Massive
         static void Main(string[] args)
         {
             Random random = new Random();
-            int[,] myArray = new int[3,5];
-            int sumSecondLine = 0;
-            int multiplyFirstColumn = 1;
-
+            int[,] myArray = new int[10,10];
+            int maxArrayNumber = myArray[0, 0];
             for (int i = 0; i < myArray.GetLength(0); i++)
             {
                 for (int j = 0; j < myArray.GetLength(1); j++)
                 {
                     myArray[i, j] = random.Next(0, 10);
-                    if (i == 1)
+                    if (myArray[i, j] > maxArrayNumber)
                     {
-                        sumSecondLine += myArray[i, j];
-                    }
-                    if(j == 0)
-                    {
-                        multiplyFirstColumn *= myArray[i, j];
+                        maxArrayNumber = myArray[i, j];
                     }
                 }
              }
-             Console.WriteLine($"Сумма второй строки {sumSecondLine} произведение первого столбца {multiplyFirstColumn}");
-         }
+            
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                   if (myArray[i, j] == maxArrayNumber)
+                    {
+                        myArray[i, j] = 0;
+                    }
+                }
+            }
+        }
     }
 }
