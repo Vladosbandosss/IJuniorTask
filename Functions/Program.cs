7e9638319b442c69d331769a95a249d62ec15600
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-07 13:55:35 +0300

    task

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -6,107 +6,30 @@ namespace Functions
     {
         static void Main(string[] args)
         {
-            bool isWorking = true;
-           
-            string[] fioInfos = new string[0];
-            string[] positionInfos = new string[0];
-          
-
-            while (isWorking)
+            Console.WriteLine("Введи число в % от 1 до 100");
+            try
             {
-                Console.WriteLine("Выбери что нужно сделать!1-добавим досье,2 выведем все досье,3 удалю досье,4 поиск по фамилии,5 выход");
-                try
-                {
-                    int ourChoise = Convert.ToInt32(Console.ReadLine());
-                    MakeTask(ref isWorking, ref fioInfos, ref positionInfos, ourChoise);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine(" Это не число");
-                }
+                DrawSymbol();
             }
-            Console.WriteLine("Программа завершена,всего доброго");
-        }
-
-        private static void MakeTask(ref bool isWorking, ref string[] fioArray, ref string[] positionArray, int ourChoise)
-        {
-            switch (ourChoise)
+            catch (FormatException)
             {
-                case 1:
-                   Console.WriteLine("Вы выбрали добавить досье!");
-                    Console.WriteLine("Введите фио");
-                    string fio = Console.ReadLine();
-                    AddInfo(ref fioArray, fioArray.Length + 1, fio);
-                    Console.WriteLine("Введите должность");
-                    string position = Console.ReadLine();
-                    AddInfo(ref positionArray, positionArray.Length + 1, position);
-                    Console.WriteLine("Готово,данные добавлены");
-                    break;
-                case 2:
-                    Console.WriteLine("Вы выбрали вывести досье,вывожу");
-                    int lenghtAllArray = fioArray.Length;
-                    for (int i = 0; i < lenghtAllArray; i++)
-                    {
-                       Console.WriteLine("Позиция " + i + " " + fioArray[i] + " - " + positionArray[i]);
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine("Удаляю последнее досье");
-                    DeleteInfo(ref fioArray, fioArray.Length - 1);
-                    DeleteInfo(ref positionArray, positionArray.Length - 1);
-                    break;
-                case 4:
-                    Console.WriteLine("Будем искать по фамилии,введите фамилию");
-                    string surname = Console.ReadLine();
-                    for (int i = 0; i < fioArray.Length; i++)
-                    {
-                        if (fioArray[i].ToLower().Contains(surname.ToLower()))
-                        {
-                            Console.WriteLine("человек найден,вывожу досье");
-                            Console.WriteLine("Полное ФИО");
-                            Console.WriteLine(fioArray[i]);
-                            Console.WriteLine("Должность");
-                            Console.WriteLine(positionArray[i]);
-                        }
-                    }
-                    break;
-                case 5:
-                    Console.WriteLine("Вы нажали выход");
-                    isWorking = false;
-                    break;
-                default:
-                    Console.WriteLine("Ошибка выбора,повтори");
-                    break;
+                Console.WriteLine("Что-то пошло не так");
             }
         }
 
-        public static void AddInfo(ref string[] arr, int newSize, string fio)
+        private static void DrawSymbol()
         {
-            string[] newArray = new string[newSize];
-            for (int i = 0; i < arr.Length; i++)
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int programDrawing = userInput / 10;
+            Console.WriteLine(programDrawing);
+            char SyblolForDrawing = '#';
+            Console.Write("[");
+            for (int i = 0; i < programDrawing; i++)
             {
-                MyArrCheck(arr, fio, newArray, i);
+                Console.Write(SyblolForDrawing);
             }
-            arr = newArray;
-        }
-
-        private static void MyArrCheck(string[] arr, string fio, string[] newArray, int i)
-        {
-            if (i == arr.Length - 1)
-            {
-                newArray[i + 1] = fio;
-            }
-            newArray[i] = arr[i];
-        }
-
-        public static void DeleteInfo(ref string[] arr, int newSize)
-        {
-            string[] newArray = new string[newSize];
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                newArray[i] = arr[i];
-            }
-            arr = newArray;
+            Console.Write("]");
         }
     }
 }
+
