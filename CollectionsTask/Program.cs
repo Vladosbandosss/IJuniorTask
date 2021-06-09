4f7c1d69a9fa4dc2e47fe8762c5d3c8fb5d59497
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-09 21:36:45 +0300

    task1

--- CollectionsTask/Program.cs
+++ CollectionsTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CollectionsTask
 {
@@ -6,7 +7,32 @@ namespace CollectionsTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task");
+            bool isWorking = true;
+            Dictionary<string, int> vocabulary = new Dictionary<string, int>();
+            vocabulary.Add("a",1);
+            vocabulary.Add("b",2);
+            vocabulary.Add("c",3);
+            vocabulary.Add("d",4);
+            vocabulary.Add("e",5);
+            vocabulary.Add("f",6);
+            vocabulary.Add("g",7);
+            vocabulary.Add("h",8);
+            vocabulary.Add("i",9);
+
+            while (isWorking)
+            {
+                Console.WriteLine("Введи слова,поищу значение");
+                string userWord = Console.ReadLine();
+
+                if (vocabulary.ContainsKey(userWord))
+                {
+                    Console.WriteLine("Все хорошо,вот значение " + vocabulary[userWord]);
+                }
+                else
+                {
+                    Console.WriteLine("нет такого значения");
+                }
+            }
         }
     }
 }
