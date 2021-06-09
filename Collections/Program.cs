1d15e872aab93b6667202db1fee5d6572dadd2be
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-09 19:45:35 +0300

    task

---
+++ Collections/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+          листы
+            List<int> numbers = new List<int>(3);
+            numbers.Add(5);
+            numbers.Add(3);
+            numbers.Add(1);
+            numbers.Add(7);
+            numbers.Add(25);
+            numbers.AddRange(new int[] { 5, 3, 4 });
+            numbers.RemoveAt(3);//уд по индексу
+            numbers.Remove(5);//уд первую 5
+            //numbers.Clear();уд полностью
+            numbers.IndexOf(25);//выведет на каком индексе 25 стоит
+            numbers.Insert(0, 123);//теперь на 0 индексе 123 будет
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            queue очередь первый вошел первый вышел
+            Queue<string> patient = new Queue<string>();
+            patient.Enqueue("Vasa");
+            patient.Enqueue("Alex");
+            patient.Enqueue("Roman");
+            patient.Enqueue("Vladimir");
+
+            foreach (var item in patient)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Now going " + patient.Dequeue());//delete him
+
+            foreach (var item in patient)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("now going" + patient.Peek());//show without delete
+
+            стэк последжний вошел первый вышел
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Test");
+            while (numbers.Count > 0)
+            {
+                Console.WriteLine("next going" + numbers.Pop());
+
+              dictionary ключ значение
+            Dictionary<string, string> country = new Dictionary<string, string>();
+            country.Add("Россия", "Москва");
+            country.Add("Украина", "Киев");
+            country.Add("Беларусь", "Минск");
+
+
+            if (country.ContainsKey("Россия"))
+            {
+                Console.WriteLine(country["Россия"]);
+            }
+
+            foreach (var item in country)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+            foreach (var item in country.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            country.Remove("Украина");
+            */
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ CollectionsTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task");
+        }
+    }
+}
