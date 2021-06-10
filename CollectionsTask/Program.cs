cba55d20b41ac0c553313d521e8991e040604959
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-10 13:13:31 +0300

    fail try

--- CollectionsTask/Program.cs
+++ CollectionsTask/Program.cs
@@ -9,32 +9,84 @@ namespace CollectionsTask
         static void Main(string[] args)
         {
             bool isWorking = true;
-            List<int> numbers = new List<int>();
+            var workers = new Dictionary<string, string>();
 
             while (isWorking)
             {
-                Console.WriteLine("Вводи числа,введи exit что б выйти с программы,введи sum и я все выведу");
-                string result = Console.ReadLine();
-                int userNumber;
-                int sum;
+                int number;
+                Console.WriteLine("Нажмите 1 что б добавить досье,2 вывести,3 удалить,4 выход");
+                string userInput = Console.ReadLine();
+                bool result = int.TryParse(userInput, out number);
 
-                if (result == "exit")
+                if (result == true)
                 {
-                    isWorking = false;
-                }
-                else if (result == "sum")
-                {
-                    sum = numbers.Sum();
-                    Console.WriteLine("Сумма всех элементов в массиве- " + sum);
-                }
-                else
-                {
-                    if (int.TryParse(result, out userNumber))
+                    switch (number)
                     {
-                        numbers.Add(userNumber);
+                        case 1:
+                            Add(workers);
+                            break;
+                        case 2:
+                            Console.WriteLine("Вывожу все досье");
+
+                            foreach (var item in workers)
+                            {
+                                Console.WriteLine(item.Key + " - " + item.Value);
+                            }
+
+                            break;
+                        case 3:
+                            RevomeWorkers(workers);
+                            break;
+                        case 4:
+                            isWorking = false;
+                            break;
+                        default:
+                            Console.WriteLine("Вы ввели что то не то");
+                            break;
                     }
                 }
             }
+            Console.WriteLine("Работа завершена");
+        }
+
+        private static void RevomeWorkers(Dictionary<string, string> workers)
+        {
+            Console.WriteLine("Удаляем досье");
+            int count = 1;
+            foreach (var item in workers)
+            {
+                Console.WriteLine(count + " " + item.Key + " - " + item.Value);
+                count++;
+            }
+
+            Console.WriteLine("Выберите номер который хотите удалить!");
+            string userChoise = Console.ReadLine();
+            int choise;
+            bool isNormalConvertion = int.TryParse(userChoise, out choise);
+
+            if (isNormalConvertion && choise > 0 && choise <= workers.Count)
+            {
+                int prepareForDelete = choise - 1;
+                for (int i = 0; i < workers.Count; i++)
+                {
+                    if (i == prepareForDelete)
+                    {
+                        
+                        //workers.Remove(); без понятия как удалить(((((
+                    }
+
+                }
+            }
+        }
+
+        private static void Add(Dictionary<string, string> workers)
+        {
+            Console.WriteLine("Добавляем!Введите ФИО");
+            string fio = Console.ReadLine();
+            Console.WriteLine("Введите должность");
+            string position = Console.ReadLine();
+            workers.Add(fio, position);
+            Console.WriteLine("Добавил");
         }
     }
 }
