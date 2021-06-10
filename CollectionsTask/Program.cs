7697f6426684baa00ecbe4c1e295f08acb158e5b
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-10 07:16:23 +0300

    правка

--- CollectionsTask/Program.cs
+++ CollectionsTask/Program.cs
@@ -9,39 +9,32 @@ namespace CollectionsTask
         static void Main(string[] args)
         {
             bool isWorking = true;
-            List<int> userArray = new List<int>();
+            List<int> numbers = new List<int>();
 
             while (isWorking)
             {
-                isWorking = WorkMoment(isWorking, userArray);
-            }
-        }
+                Console.WriteLine("Вводи числа,введи exit что б выйти с программы,введи sum и я все выведу");
+                string result = Console.ReadLine();
+                int userNumber;
+                int sum;
 
-        private static bool WorkMoment(bool isWorking, List<int> userArray)
-        {
-            Console.WriteLine("Вводи числа,введи exit что б выйти с программы,введи sum и я все выведу");
-            string result = Console.ReadLine();
-            int userNumber;
-            int sum;
-
-            if (result == "exit")
-            {
-                isWorking = false;
-            }
-            else if (result == "sum")
-            {
-                sum = userArray.Sum();
-                Console.WriteLine("Сумма всех элементов в массиве- " + sum);
-            }
-            else
-            {
-                if (int.TryParse(result, out userNumber))
+                if (result == "exit")
                 {
-                    userArray.Add(userNumber);
+                    isWorking = false;
+                }
+                else if (result == "sum")
+                {
+                    sum = numbers.Sum();
+                    Console.WriteLine("Сумма всех элементов в массиве- " + sum);
+                }
+                else
+                {
+                    if (int.TryParse(result, out userNumber))
+                    {
+                        numbers.Add(userNumber);
+                    }
                 }
             }
-
-            return isWorking;
         }
     }
 }
