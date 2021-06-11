5f220fb1a7984ae1b6b9790303f97efe6d573f76
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-11 14:38:43 +0300

    task1

---
+++ OOP/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _damage;
+
+        public Player(string name,int health,int damage)
+        {
+            _name = name;
+            _health = health;
+            _damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"имя {_name} здоровье {_health} урон {_damage}");
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player myPlayer = new Player("Влад", 100, 50);
+            myPlayer.ShowInfo();
+        }
+    }
+}
