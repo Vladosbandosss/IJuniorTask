fb7060439f018a1c09f778d4472e40a974d14ee6
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-15 21:23:13 +0300

    taskfight

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,154 +7,182 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            bool isWorking = true;
-            Seller seller = new Seller(CreateGoods());
-            Costumer costumer = new Costumer("Павел", 100);
+            Arene arene = new Arene();
             
-            while (isWorking)
+            arene.AddWarriots(CreateWarriors());
+           
+            bool isBattleArene = true;
+
+            while (isBattleArene)
             {
-                Console.WriteLine("Добро пожаловать в магазин");
-                Console.WriteLine("Нажми 1 что б купить товар,нажми 2,что б посмотреть свои товары,3 что б выйти");
-                int userInput;
-                int.TryParse(Console.ReadLine(), out userInput);
+                int userInput = AreneFight(arene);
 
                 switch (userInput)
                 {
                     case 1:
-                        BuyGoods(seller, costumer);
+                        isBattleArene = true;
                         break;
                     case 2:
-                        Console.WriteLine("Давай посмотрим что у тебя есть");
-                        costumer.ShowCostumerGoods();
-                        break;
-                    case 3:
-                        isWorking = false;
+                        isBattleArene = false;
                         break;
                     default:
-                        Console.WriteLine("Что-то не то");
+                        Console.WriteLine("Что то не то");
                         break;
                 }
             }
             Console.WriteLine("Программа завершена");
-
         }
 
-        private static void BuyGoods(Seller seller, Costumer costumer)
+        private static int AreneFight(Arene arene)
         {
-            Console.WriteLine("Ты выбрал купить товары");
-            seller.ShowSellerGoods();
-            Console.WriteLine("У тебя есть " + costumer.ShowCostumerMoney());
-            Console.WriteLine("Выбери товар который хочешь купить");
-            int userWantBuy;
-            int.TryParse(Console.ReadLine(), out userWantBuy);
-            int finalUserDessicion = userWantBuy - 1;
-            costumer.BuyGoods(CreateGoods(), finalUserDessicion);
-            if (costumer.ShowCostumerMoney() >= seller.GetGoodPrice(finalUserDessicion))
-            {
-                seller.SellGoods(finalUserDessicion);
-            }
+            Console.WriteLine("Добро пожаловать на бой!Вот список бойцов");
+            arene.ShowWarriotInfo();
+            Console.WriteLine("Выбери 2 бойцов");
+            Console.WriteLine("Выбери первого бойца");
+            int userFirstChoose;
+            int.TryParse(Console.ReadLine(), out userFirstChoose);
+            Console.WriteLine("Выбери второго бойца");
+            int userSecondChpoise;
+            int.TryParse(Console.ReadLine(), out userSecondChpoise);
+            arene.Fight(arene.GetWarrior(userFirstChoose), arene.GetWarrior(userSecondChpoise));
+
+            Console.WriteLine("Желаете повторить?1-да 2-нет");
+            int userInput;
+            int.TryParse(Console.ReadLine(), out userInput);
+            return userInput;
         }
 
-        public static List<Goods> CreateGoods()
+        private static List<Warrior> CreateWarriors()
         {
-            List<Goods> allGoods = new List<Goods>();
-            allGoods.Add (new Goods("Мяч", 50));
-            allGoods.Add(new Goods("Компьютер", 200));
-            allGoods.Add(new Goods("Чашка", 20));
-            allGoods.Add(new Goods("Ручка", 10));
-            allGoods.Add(new Goods("Кофе", 100));
-            return allGoods;
+            List<Warrior> creatWarrior = new List<Warrior>();
+            Warrior Vlad = new Warrior("Влад", 100, 20, 10, 10);
+            creatWarrior.Add(Vlad);
+            Warrior Roma = new Warrior("Рома", 100, 10, 10, 5);
+            creatWarrior.Add(Roma);
+            Warrior Pashe = new Warrior("Паша", 75, 20, 5, 7);
+            creatWarrior.Add(Pashe);
+            Warrior Zoy = new Warrior("Зой", 125, 23, 8, 1);
+            creatWarrior.Add(Zoy);
+            Warrior Edvard = new Warrior("Эдвард", 90, 5, 5, 20);
+            creatWarrior.Add(Edvard);
+
+            return creatWarrior;
         }
     }
-   class Seller
+    class Hero
     {
-       private List<Goods> _goodsSellerList;
+        protected int Health;
+        protected int Damage;
+        protected int Armor;
+        protected string Name;
+       
+        public Hero(string name ,int health,int damage,int armor) {
+            Name = name;
+            Health = health;
+            Damage = damage;
+            Armor = armor;
+        }
 
-        public Seller(List<Goods> goodList)
+        public int GetHealth()
         {
-            _goodsSellerList = goodList;
+            return Health;
         }
-        public int GetGoodPrice(int index)
+        public int GetDamage()
         {
-            return _goodsSellerList[index].GetPriceGood();
+            return Damage;
         }
-        public void ShowSellerGoods()
+        public int GetArmor()
         {
-            for (int i = 0; i < _goodsSellerList.Count; i++)
-            {
-                Console.Write("Позиция " + (i + 1) + "  ");
-                _goodsSellerList[i].ShowGoodsDescription();
-            }
+            return Armor;
         }
-        public void SellGoods(int userInput)
+        public void ShowDescription()
         {
-            for (int i = 0; i <=_goodsSellerList.Count; i++)
-            {
-                if (userInput == i )
-                {
-                    _goodsSellerList.Remove(_goodsSellerList[userInput]);
-                }
-            }
+            Console.WriteLine($"имя {Name} здоровье {Health} урон {Damage} броня {Armor} ,а супер сила секрет");
         }
     }
-    class Costumer
-    {
-        private string _name;
-        private int _money;
-        private List<Goods> _costumerGoods = new List<Goods>();
 
-        public Costumer(string name,int money)
+    class Warrior : Hero
+    {
+        protected int WarriorSuperPower;
+        public Warrior(string WariorName, int WariorHealth,int WariorDamage,int WariorArmor,int superPower):base(WariorName,WariorHealth,WariorDamage,WariorArmor)
         {
-            _name = name;
-            _money = money;
+            WarriorSuperPower = superPower;
         }
-        public int ShowCostumerMoney()
+
+        public int GetSuperPower()
         {
-            return _money;
+            Random random = new Random();
+            int superPower = random.Next(1, 11);
+            return WarriorSuperPower+superPower;
         }
-        public void ShowCostumerGoods()
+    }
+    
+    class Arene
+    {
+        List<Warrior> listWarriors = new List<Warrior>();
+
+        public void AddWarriots(List<Warrior>warriors)
         {
-            for (int i = 0; i < _costumerGoods.Count; i++)
+            listWarriors.AddRange(warriors);
+        }
+
+        public Warrior GetWarrior(int userInput)
+        {
+            if (userInput - 1 >= 0 && userInput - 1 < listWarriors.Count)
             {
-               _costumerGoods[i].ShowGoodsDescription();
-            }
-        }
-        public void BuyGoods(List<Goods>list,int userChoise)
-        {
-           
-            if (_money >=list[userChoise].GetPriceGood())
-            {
-                _costumerGoods.Add(list[userChoise]);
-                _money -= list[userChoise].GetPriceGood();
+                return listWarriors[userInput - 1];
             }
             else
             {
-                Console.WriteLine("Недостаточно денег");
-               
+                Console.WriteLine("Ошибка выбора я сам поставлю бой");
+                return listWarriors[0];
             }
            
         }
-    }
-    class Goods
-    {
-        private string _name;
-        private int _price;
-        
-        public Goods(string name,int price)
+        public void ShowWarriotInfo()
         {
-            _name = name;
-            _price = price;
+            for (int i = 0; i < listWarriors.Count; i++)
+            {
+                Console.Write("боец" + (i+1) + " ");
+                listWarriors[i].ShowDescription();
+            }
         }
-        public void ShowGoodsDescription()
+
+       public void Fight(Warrior firstWarrior,Warrior secondWarrior)
         {
-            Console.WriteLine($"Товар {_name} цена {_price}"); 
-        }
-        public int GetPriceGood()
-        {
-            return _price;
-        }
+            int firstWarriorHealth = firstWarrior.GetHealth();
+            int secondWarriorHealth = secondWarrior.GetHealth();
+            bool isFight= true;
+            
+            while (isFight)
+                {
+                    Console.WriteLine("Идет бой между двумя героями");
+                    Console.WriteLine($"У первого героя {firstWarriorHealth} здоровья");
+                    Console.WriteLine($"У второго героя {secondWarriorHealth} здоровья");
+                    firstWarriorHealth -= (secondWarrior.GetDamage() + secondWarrior.GetSuperPower()) + firstWarrior.GetArmor();
+                    secondWarriorHealth -= (firstWarrior.GetDamage() + firstWarrior.GetSuperPower()) + secondWarrior.GetArmor();
+
+                    if (firstWarriorHealth <= 0 && secondWarriorHealth <= 0)
+                    {
+                        Console.WriteLine("Ничья");
+                        isFight = false;
+                    }
+                    if (firstWarriorHealth > 0 && secondWarriorHealth <= 0)
+                    {
+                        Console.WriteLine("Победил первый герой");
+                        isFight = false;
+                    }
+                    if (firstWarriorHealth <= 0 && secondWarriorHealth > 0)
+                    {
+                        Console.WriteLine("Победил второй игрок");
+                        isFight = false;
+                    }
+            }
+          Console.WriteLine("Бой окончен");
+       }
     }
 }
+
 
    
 
