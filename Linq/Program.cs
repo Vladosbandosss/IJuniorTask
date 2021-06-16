334b8590a433c6fc9f7bac4f42175541e0aec515
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-16 14:12:32 +0300

    task1

---
+++ Linq/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Murder> murdersList = new List<Murder>();
+            murdersList.AddRange(CreateMurders());
+
+            FindMurders(murdersList);
+
+        }
+
+        private static void FindMurders(List<Murder> murdersList)
+        {
+            Console.WriteLine("Добро пожаловать в поиск!");
+            Console.WriteLine("Введите рост преступника");
+            int hightMurder;
+            int.TryParse(Console.ReadLine(), out hightMurder);
+            Console.WriteLine("Введите возраст преступника");
+            int ageMurder;
+            int.TryParse(Console.ReadLine(), out ageMurder);
+            Console.WriteLine("Введите национлаьность");
+            string nationalityMurser = Console.ReadLine();
+
+            var userResult = murdersList.Where(murderSearch => murderSearch.GerArest() == false)
+                .Where(murderSearch => murderSearch.GetAge() == ageMurder)
+                .Where(murderSearch => murderSearch.GetHeight() == hightMurder)
+                .Where(murderSearch => murderSearch.GetNation() == nationalityMurser).ToArray();
+
+            if (userResult.Count() > 0)
+            {
+                foreach (var item in userResult)
+                {
+                    item.ShowMurderDesctiption();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Данные не найдены");
+            }
+        }
+
+        private static List<Murder> CreateMurders()
+        {
+            List<Murder> mursderList = new List<Murder>();
+            Murder firstMurder = new Murder("Vlad", true, 190, 20, "Belarus");
+            mursderList.Add(firstMurder);
+            Murder secondMurder = new Murder("Pavel", false, 180, 30, "Ukraine");
+            mursderList.Add(secondMurder);
+            Murder forthMurder = new Murder("Vova", false, 150, 29, "Belarus");
+            mursderList.Add(forthMurder);
+            Murder fifthMurder = new Murder("Zoy", false, 180, 30, "Belarus");
+            mursderList.Add(fifthMurder);
+            Murder sixMurder = new Murder("Sasha", true, 145, 50, "Russia");
+            mursderList.Add(sixMurder);
+            return mursderList;
+        }
+    }
+    class Murder
+    {
+        private string _fio;
+        private bool _isArested;
+        private int _height;
+        private int _age;
+        private string _nation;
+
+        public Murder(string fio,bool isArest,int height,int age,string nation)
+        {
+            _fio = fio;
+            _isArested = isArest;
+            _height = height;
+            _age = age;
+            _nation = nation;
+        }
+        public void ShowMurderDesctiption()
+        {
+            Console.WriteLine($"ФИО {_fio} статус ареста {_isArested} рост {_height} возраст {_age} национальность {_nation} ");
+        }
+        public string GetFio()
+        {
+            return _fio;
+        }
+
+        public bool GerArest()
+        {
+            return _isArested;
+        }
+        public int GetAge()
+        {
+            return _age;
+        }
+        public int GetHeight()
+        {
+            return _height;
+        }
+        public string GetNation()
+        {
+            return _nation;
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OOP
 {
@@ -7,179 +8,25 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Arena arene = new Arena();
-            
-            arene.AddWarriots(CreateWarriors());
-           
-            bool isBattleArene = true;
+            Console.WriteLine("Вводи число");
+            string userInput = Console.ReadLine();
+            int[] userMas = new int[5];
 
-            while (isBattleArene)
+            for (int i = 0; i < userInput.Length; i++)
             {
-                int userInput = FightArena(arene);
-
-                switch (userInput)
-                {
-                    case 1:
-                        isBattleArene = true;
-                        break;
-                    case 2:
-                        isBattleArene = false;
-                        break;
-                    default:
-                        Console.WriteLine("Что то не то");
-                        break;
-                }
+                Console.WriteLine(userInput[i]);
+                userMas[i] = Convert.ToInt32( userInput[i].ToString());
             }
-            Console.WriteLine("Программа завершена");
-        }
 
-        private static int FightArena(Arena arene)
-        {
-            Console.WriteLine("Добро пожаловать на бой!Вот список бойцов");
-            arene.ShowWarriotInfo();
-            Console.WriteLine("Выбери 2 бойцов");
-            Console.WriteLine("Выбери первого бойца");
-            int userFirstChoose;
-            int.TryParse(Console.ReadLine(), out userFirstChoose);
-            Console.WriteLine("Выбери второго бойца");
-            int userSecondChpoise;
-            int.TryParse(Console.ReadLine(), out userSecondChpoise);
-            arene.Fight(arene.GetWarrior(userFirstChoose), arene.GetWarrior(userSecondChpoise));
-
-            Console.WriteLine("Желаете повторить?1-да 2-нет");
-            int userInput;
-            int.TryParse(Console.ReadLine(), out userInput);
-            return userInput;
-        }
-
-        private static List<Warrior> CreateWarriors()
-        {
-            List<Warrior> creatWarrior = new List<Warrior>();
-            Warrior Vlad = new Warrior("Влад", 100, 20, 10, 10);
-            creatWarrior.Add(Vlad);
-            Warrior Roma = new Warrior("Рома", 100, 10, 10, 5);
-            creatWarrior.Add(Roma);
-            Warrior Pashe = new Warrior("Паша", 75, 20, 5, 7);
-            creatWarrior.Add(Pashe);
-            Warrior Zoy = new Warrior("Зой", 125, 23, 8, 1);
-            creatWarrior.Add(Zoy);
-            Warrior Edvard = new Warrior("Эдвард", 90, 5, 5, 20);
-            creatWarrior.Add(Edvard);
-
-            return creatWarrior;
-        }
-    }
-    class Hero
-    {
-        protected int Health;
-        protected int Damage;
-        protected int Armor;
-        protected string Name;
-       
-        public Hero(string name ,int health,int damage,int armor) {
-            Name = name;
-            Health = health;
-            Damage = damage;
-            Armor = armor;
-        }
-
-        public int GetHealth()
-        {
-            return Health;
-        }
-        public int GetDamage()
-        {
-            return Damage;
-        }
-        public int GetArmor()
-        {
-            return Armor;
-        }
-        public void ShowDescription()
-        {
-            Console.WriteLine($"имя {Name} здоровье {Health} урон {Damage} броня {Armor} ,а супер сила секрет");
-        }
-    }
-
-    class Warrior : Hero
-    {
-        protected int WarriorSuperPower;
-        public Warrior(string WariorName, int WariorHealth,int WariorDamage,int WariorArmor,int superPower):base(WariorName,WariorHealth,WariorDamage,WariorArmor)
-        {
-            WarriorSuperPower = superPower;
-        }
-
-        public int GetSuperPower()
-        {
-            Random random = new Random();
-            int superPower = random.Next(1, 11);
-            return WarriorSuperPower+superPower;
-        }
-    }
-    
-    class Arena
-    {
-        List<Warrior> listWarriors = new List<Warrior>();
-
-        public void AddWarriots(List<Warrior>warriors)
-        {
-            listWarriors.AddRange(warriors);
-        }
-
-        public Warrior GetWarrior(int userInput)
-        {
-            if (userInput - 1 >= 0 && userInput - 1 < listWarriors.Count)
+            foreach (var item in userMas)
             {
-                return listWarriors[userInput - 1];
+                Console.WriteLine(item);
             }
-            else
-            {
-                Console.WriteLine("Ошибка выбора я сам поставлю бой");
-                return listWarriors[0];
-            }
-           
-        }
-        public void ShowWarriotInfo()
-        {
-            for (int i = 0; i < listWarriors.Count; i++)
-            {
-                Console.Write("боец" + (i+1) + " ");
-                listWarriors[i].ShowDescription();
-            }
-        }
+            int sum = userMas.Sum();
 
-       public void Fight(Warrior firstWarrior,Warrior secondWarrior)
-        {
-            int firstWarriorHealth = firstWarrior.GetHealth();
-            int secondWarriorHealth = secondWarrior.GetHealth();
-            bool isFight= true;
-            
-            while (isFight)
-                {
-                    Console.WriteLine("Идет бой между двумя героями");
-                    Console.WriteLine($"У первого героя {firstWarriorHealth} здоровья");
-                    Console.WriteLine($"У второго героя {secondWarriorHealth} здоровья");
-                    firstWarriorHealth -= (secondWarrior.GetDamage() + secondWarrior.GetSuperPower()) + firstWarrior.GetArmor();
-                    secondWarriorHealth -= (firstWarrior.GetDamage() + firstWarrior.GetSuperPower()) + secondWarrior.GetArmor();
-
-                    if (firstWarriorHealth <= 0 && secondWarriorHealth <= 0)
-                    {
-                        Console.WriteLine("Ничья");
-                        isFight = false;
-                    }
-                    if (firstWarriorHealth > 0 && secondWarriorHealth <= 0)
-                    {
-                        Console.WriteLine("Победил первый герой");
-                        isFight = false;
-                    }
-                    if (firstWarriorHealth <= 0 && secondWarriorHealth > 0)
-                    {
-                        Console.WriteLine("Победил второй игрок");
-                        isFight = false;
-                    }
-            }
-          Console.WriteLine("Бой окончен");
+            Console.WriteLine(sum);
        }
+        
     }
 }
 
