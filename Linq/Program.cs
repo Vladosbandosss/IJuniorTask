ecfd034a420d458c5ebdec2b6753cff0376734ea
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-16 16:26:15 +0300

    task3

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -8,61 +8,99 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            List<Person> persons = new List<Person>();
-            persons.AddRange(CreatePersons());
-            Console.WriteLine("Люди в тюрьме"); 
+            List<Ill> illPerson = new List<Ill>();
+            AddIllPerson(illPerson);
 
-            for (int i = 0; i < persons.Count; i++)
+            bool isWorking = true;
+
+            while (isWorking)
             {
-                persons[i].ShowDescription();
+                Console.WriteLine("Добро пожаловать в больничный список,что хотите сделать?1-отсортировать по фио,2-отсортировать по возрасту," +
+                    "3 вывести заболевание, 4 выход");
+                int userInput;
+                int.TryParse(Console.ReadLine(), out userInput);
+
+                switch (userInput)
+                {
+                    case 1:
+                        var fioSort = illPerson.Where(ill => ill.GetAge() > 0).OrderBy(x => x.GetFio()).ToList();
+                        ShowInfo(fioSort);
+                        break;
+                    case 2:
+                        var ageSort = illPerson.Where(ill => ill.GetAge() > 0).OrderBy(x => x.GetAge()).ToList();
+                        ShowInfo(ageSort);
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите болезнь,поищу");
+                        string illness = Console.ReadLine();
+                        var illSort = illPerson.Where(ill => ill.GetIll().Contains(illness)).ToList();
+                        ShowInfo(illSort);
+                        break;
+                    case 4:
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка выбора");
+                        break;
+                }
             }
 
-            var amnistiaMurders = persons.Where(prest => prest.GetMurderInfo() != "Антиправительственное").ToList();
+            Console.WriteLine("Завершил");
+        }
 
-            Console.WriteLine("Вот список людей которые остануться в тюрьме");
-            foreach (var item in amnistiaMurders)
+        private static void ShowInfo(List<Ill> fioSort)
+        {
+            foreach (var item in fioSort)
             {
                 item.ShowDescription();
             }
-
-
         }
 
-        private static List<Person> CreatePersons()
+        private static void AddIllPerson(List<Ill> illPerson)
         {
-            List<Person> personList = new List<Person>();
-            Person firstPerson = new Person("Vlad","Воровство");
-            personList.Add(firstPerson);
-            Person secondPerson = new Person("Pasha","Воровство");
-            personList.Add(secondPerson);
-            Person thirdPerson = new Person("Roma","Антиправительственное");
-            personList.Add(thirdPerson);
-            Person fourthPerson = new Person("Sasha","Антиправительственное");
-            personList.Add(fourthPerson);
-            Person fifthPerson = new Person("Katya","Антиправительственное");
-            personList.Add(fifthPerson);
-            return personList;
+            illPerson.Add(new Ill("Влад", 20, "Понос"));
+            illPerson.Add(new Ill("Рома", 25, "Перелом"));
+            illPerson.Add(new Ill("Вадим", 30, "Понос"));
+            illPerson.Add(new Ill("Роман", 18, "Укус клеща"));
+            illPerson.Add(new Ill("Саша", 60, "Перелом"));
+            illPerson.Add(new Ill("Даша", 27, "Ушиб"));
+            illPerson.Add(new Ill("Слава", 22, "Насморк"));
+            illPerson.Add(new Ill("Вячеслав", 33, "Столбняк"));
+            illPerson.Add(new Ill("Павел", 48, "Дифтерия"));
+            illPerson.Add(new Ill("Ян", 32, "Перелом"));
         }
     }
-    class Person
+    class Ill
     {
-        private string _name;
-        private string _murderInfo;
+        private string _fio;
+        private int _age;
+        private string _illName;
 
-        public Person(string name,string murderInfo)
+        public Ill(string fio,int age,string illName)
         {
-            _name = name;
-            _murderInfo = murderInfo;
+            _fio = fio;
+            _age = age;
+            _illName = illName;
+        }
+
+        public string GetFio()
+        {
+            return _fio;
+        }
+        
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        public string GetIll()
+        {
+            return _illName;
         }
 
         public void ShowDescription()
         {
-         Console.WriteLine($"{_name} преступление  {_murderInfo}");
-        }
-
-        public string GetMurderInfo()
-        {
-            return _murderInfo;
+            Console.WriteLine($"пациент {_fio} возраст {_age} болезнь {_illName}");
         }
     }
 }
