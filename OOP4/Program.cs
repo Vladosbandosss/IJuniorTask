251142d1592341920797b13ee1cb4702d76f93eb
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-06-19 10:54:02 +0300

    shopTask

---
+++ OOP4/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isShopWorking = true;
+
+            Shop shop = CreateCustomers();
+
+            while (isShopWorking)
+            {
+                Console.WriteLine("Добро пожаловать в магазин!Нажми 1 что б обслужить людей,2 что б выйти");
+                int userInput;
+                int.TryParse(Console.ReadLine(), out userInput);
+
+                switch (userInput)
+                {
+                    case 1:
+                        shop.MakeSale();
+                        break;
+                    case 2:
+                        Console.WriteLine("Пока");
+                        isShopWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+            }
+        }
+
+        private static Shop CreateCustomers()
+        {
+            Product productChease = new Product("Сыр", 100);
+            Product productMilk = new Product("Молоко", 150);
+            Product productCoffe = new Product("Кофе", 50);
+            Product productTea = new Product("Чай", 60);
+            Product productPC = new Product("Компьютер", 200);
+            Product productNapkin = new Product("Салфетка", 80);
+
+            List<Product> productsFirst = new List<Product>
+            {
+                productChease,
+                productCoffe
+            };
+
+            Customer customer = new Customer(250, productsFirst);
+
+            List<Product> productSecond = new List<Product>
+           {
+               productChease,
+               productMilk,
+               productCoffe
+           };
+
+            Customer secondCustomer = new Customer(200, productSecond);
+
+            List<Customer> customerList = new List<Customer>();
+            customerList.Add(customer);
+            customerList.Add(secondCustomer);
+
+            Shop shop = new Shop(customerList);
+            return shop;
+        }
+    }
+    class Product
+    {
+        private string _name;
+        private int _price;
+
+        public Product(string name,int price)
+        {
+            _name = name;
+            _price = price;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetPrice()
+        {
+            return _price;
+        }
+        public void ShowDescription()
+        {
+            Console.WriteLine($"{_name} цена {_price} р");
+        }
+    }
+    class Customer
+    {
+        private int _money;
+        private List<Product> _productsList = new List<Product>();
+
+        public Customer(int money, List<Product> products)
+        {
+            _money = money;
+            _productsList.AddRange(products);
+        }
+        public void Info()
+        {
+            Console.WriteLine($"У меня есть {_money} р");
+            Console.WriteLine("Вот мой список продуктов");
+
+            for (int i = 0; i < _productsList.Count; i++)
+            {
+                _productsList[i].ShowDescription();
+            }
+        }
+        public int GetMyMoney()
+        {
+            return _money;
+        }
+        public int PriceForGoogs()
+        {
+            int moneyForPrice = 0;
+
+            for (int i = 0; i < _productsList.Count; i++)
+            {
+                moneyForPrice += _productsList[i].GetPrice();
+            }
+
+            return moneyForPrice;
+        }
+       public void RemoveGoog()
+        {
+            Random random = new Random();
+            int revomeRroduct = random.Next(0, _productsList.Count);
+            for (int i = 0; i < _productsList.Count; i++)
+            {
+                _productsList.RemoveAt(revomeRroduct);
+            }
+        }
+    }
+    class Shop
+    {
+        private List<Customer> _customers = new List<Customer>();
+
+        public Shop(List<Customer> customers)
+        {
+            _customers.AddRange(customers);
+        }
+        
+        public void MakeSale()
+        {
+            for (int i = 0; i < _customers.Count;)
+            {
+                if (_customers[i].GetMyMoney() >= _customers[i].PriceForGoogs())
+                {
+                    Console.WriteLine("У нас есть " + _customers[i].GetMyMoney() + " сумма покупок " + _customers[i].PriceForGoogs());
+                    Console.WriteLine("Покупка прошла успешно!");
+                    _customers.RemoveAt(i);
+                    //i++;
+                }
+                else
+                {
+                   Console.WriteLine("У нас есть " + _customers[i].GetMyMoney() + " сумма покупок " + _customers[i].PriceForGoogs());
+                    Console.WriteLine("Нешта ты много набрал,ща выкину что нибудь");
+                    _customers[i].RemoveGoog();
+                    i++;
+                    MakeSale();
+                }
+            }
+        }
+    }
+}
